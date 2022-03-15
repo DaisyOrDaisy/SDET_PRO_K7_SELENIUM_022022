6667de2ecc88cb706de629770225417eeecb1139
Stellantt <nguyenthithaoqh2000@gmail.com>
2022-03-15 10:14:04 +0700

    lab06 version2

--- lab06/src/lab064/CheckUrl.java
+++ lab06/src/lab064/CheckUrl.java
@@ -2,25 +2,14 @@ package lab064;
 
 public class CheckUrl {
     public static void main(String[] args) {
-        String url = "https://google.com",protocol="",domain="";
-        int i=0;
-        // extract ten gia thuc
-        while (url.charAt(i)!=':'){
-            protocol=protocol.concat(Character.toString(url.charAt(i)));
-            i++;
-        }
-        // bo qua phan giua dia chi
-        while (url.charAt(i)!='.'){
-            i++;
-        }
-        i++;//bo qua dau '.'
-        // extract ten mien
-        while(i<url.length()){
-            domain=domain.concat(Character.toString(url.charAt(i)));
-            i++;
-        }
+        String url = "https://google.com";
+
+        String protocol=url.substring(0,url.indexOf(':'));
+        String topLevelDomain=url.substring(url.lastIndexOf('.'));
+        String domain=url.substring(url.lastIndexOf('/')+1,url.lastIndexOf('.'));
 
         System.out.println("the protocol has been used: "+protocol);
         System.out.println("the domain name :"+domain);
+        System.out.println("the top level domain :"+topLevelDomain);
     }
 }
